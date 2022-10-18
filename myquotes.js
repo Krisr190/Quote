4e4be4bd6ba0ad8623d4f0d70c051687ad375248
Krisr190 <116085296+Krisr190@users.noreply.github.com>
2022-10-18 14:36:08 +0100

    Update myquotes.js

--- myquotes.js
+++ myquotes.js
@@ -10,5 +10,17 @@
 	{
 	"text": "The biggest problem we run into is going, ‘This is who I am, this is what I’m like, this is how I function’ while failing to notice that you don’t do that anymore",
         "author": "Neil Gaiman"
+	},
+	{
+	"text": "The truth is revealed by removing things which stand in its light, an art, not unlike sculpture, in which the artist creates, not by building, but by hacking away",
+        "author": "Alan Watts"
+	},
+    {
+	"text": "Don’t ever solve the same the same problem three times in a row. If you have the same question or problem three times in a row, systematize it",
+        "author": "Joel Runyun"
+	},
+    {
+	"text": "Time is the king of all men, he is their parent and their grave, and gives them what he will and not what they crave",
+        "author": "Pericles"
 	}
 ]
